1e76c4e3c18492fa6c6e094a542daf003e1cbf15
Alexander <mr.haizenbeerg666@gmail.com>
2019-06-03 10:24:47 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Swal from 'sweetalert2'
 
 function App() {
+    function handleClick(e){
+      Swal.fire({
+       
+        type: 'info',
+        html:
+          '<p> Go through registration</p> ' +
+          '<div class="group">' +
+          '<input type="text" required/>'+
+          '<span class="bar"></span>'+
+          '<label>Record book</label>'+
+          '</div>'+
+          '<div class="group">' +
+          '<input type="text" required/>'+
+          '<span class="bar"></span>'+
+          '<label>Password</label>'+
+          '</div>',
+        showCloseButton: true,
+        showCancelButton: true,
+        focusConfirm: false,
+        confirmButtonText:
+          '<i class="fa fa-thumbs-up"></i> Great!',
+        confirmButtonAriaLabel: 'Thumbs up, great!',
+        cancelButtonText:
+          '<i class="fa fa-thumbs-down"></i> Cancel',
+        cancelButtonAriaLabel: 'Thumbs down',
+      })
+    }
+    
   return (
     <div className="App">
       <header className="App-header">
+       
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+     <form>
+     <label>Electron book of attendance @PC IUK</label>
+
+         <div class="group">
+              <input type="text" required/>
+              <span class="bar"></span>
+              <label>Record book</label>
+          </div>
+          <div class="group">
+          <input type="text" required/>
+              <span class="bar"></span>
+              <label>Password</label>
+         </div>
+         <a href="#" class="glo">Sign in</a>
+         <div id="registration">
+         {/* onClick={} */}
+        <a href="#" onClick={handleClick} class="reg">Sign up</a>
+      </div>
+      </form>
+       
       </header>
     </div>
   );
